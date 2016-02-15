b57c10283105187acbd34c41e3fda8951e0516a4
IvanStoichkov <dragon_m@abv.bg>
2016-02-15 20:32:51 +0200

    Start

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		System.out.println("hello bai-shile!");
+		System.out.println("byeee!");
+	}
+
+}
